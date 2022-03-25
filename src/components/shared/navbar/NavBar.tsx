890a852435c300dbb58c4780824243ead52eb8fc
MihiranP <mihiran.paranamana@car-care.se>
2022-03-26 04:08:42 +0530

    Add navbar and styles

--- src/components/shared/navbar/NavBar.tsx
+++ src/components/shared/navbar/NavBar.tsx
@@ -1,41 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Form} from 'react-bootstrap'
+import {Container, Navbar, Nav, Form, FormControl} from 'react-bootstrap'
+import {BsSearch} from 'react-icons/bs'
 
 import './NavBar.scss'
 
 export const NavBar: React.FC = () => {
-    const searchPlaceholder = 'Serch for items, collections, creators...';
+    const searchPlaceholder = 'Search for items, collections, creators...';
     return (
-        <header className="header">
-            <img className="logo" src={require('../../../assets/navbar-logo.png')} alt="Logo"/>
-            <Form className="w-180 ml-2">
-                <Form.Group controlId="search">
-                    <Form.Control className="w-180" type="text" placeholder={searchPlaceholder} />
-                </Form.Group>
-            </Form>
-            <nav className="navigations">
-                <ul>
-                    <li>
-                        <Link to='/explore'>Explore</Link>
-                    </li>
-                    <li>
-                        <Link to='/activity'>Activity</Link>
-                    </li>
-                    <li>
-                        <Link to='/academy'>Academy</Link>
-                    </li>
-                    <li>
-                        <Link to='/blog'>Blog</Link>
-                    </li>
-                    <li>
-                        <Link to='/sign-in'>Sign in</Link>
-                    </li>
-                    <li>
-                        <Link to='/sign-up'>Create</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <Navbar bg="light" expand="lg" sticky="top">
+            <Container fluid className="navbar-container">
+                <Navbar.Brand>
+                    <img src={require('../../../assets/navbar-logo.png')} alt="Logo"/>
+                </Navbar.Brand>
+                <Form className="search d-flex">
+                    <BsSearch className="searchIcon"/>
+                    <FormControl
+                        className="searchInput"
+                        type="search"
+                        placeholder={searchPlaceholder}
+                        aria-label="Search"/>
+                </Form>
+                <Navbar.Toggle className="ms-auto" aria-controls="navbarScroll" />
+                <div className="ms-lg-auto navigations">
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav style={{ maxHeight: '100px' }} navbarScroll>
+                            <Nav.Link className="navLink" href='/explore'>Explore</Nav.Link>
+                            <Nav.Link className="navLink" href='/activity'>Activity</Nav.Link>
+                            <Nav.Link className="navLink" href='/academy'>Academy</Nav.Link>
+                            <Nav.Link className="navLink" href='/blog'>Blog</Nav.Link>
+                            <Nav.Link className="navLink signIn" href='/sign-in'>Sign in</Nav.Link>
+                            <Nav.Link className="navLink signOut" href='/sign-up'>Create</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Container>
+        </Navbar>
     )
 }
