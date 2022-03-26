7239072fda1dec43b21d74814e5277696c3ba0fd
MihiranP <mihiran.paranamana@car-care.se>
2022-03-26 05:59:22 +0530

    Add Sign in / Sign up screens and styles

--- src/components/sign-up/SignUp.tsx
+++ src/components/sign-up/SignUp.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+import './SignUp.scss';
+
+import {Card} from '../shared/card/Card';
 
 export const SignUp: React.FC = (props) => {
     return (
-        <div>SignUp</div>
+        <Card>
+            <div className="sign-up-container text-center">
+                <h4>Hi, Let's set up your wallet!</h4>
+                <br/>
+                <Button className="btn" type="button">Select a wallet</Button>
+                <div className="mt-1">
+                    <Link className="text-decoration-none" to='/sign-up'>
+                        Is this your first time setting up a wallet?
+                    </Link>
+                </div>
+            </div>
+        </Card>
     )
 }
