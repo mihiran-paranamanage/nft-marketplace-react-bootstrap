e17481d21869b064818425bd1d621411a9104381
MihiranP <mihiran.paranamana@car-care.se>
2022-03-26 05:38:13 +0530

    Add Sign in screen and styles

--- src/components/sign-in/SignIn.tsx
+++ src/components/sign-in/SignIn.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import {Button, Form} from 'react-bootstrap';
+
+import './SignIn.scss';
+
+import {Card} from '../shared/card/Card';
+import {Link} from 'react-router-dom';
 
 export const SignIn: React.FC = (props) => {
     return (
-        <div>SignIn</div>
+        <Card>
+            <h4 className="text-center">Account Login</h4>
+            <br/>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control className="input" type="email" placeholder="Email address" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control className="input" type="password" placeholder="Password" />
+                </Form.Group>
+                <Button className="submitBtn" type="submit">Submit</Button>
+            </Form>
+            <div className="text-end mt-1">
+                <Link className="text-decoration-none" to='/sign-up'>Sign up</Link>
+                { ' / ' }
+                <Link className="text-decoration-none" to=''>Forgot your password?</Link>
+            </div>
+        </Card>
     )
 }
