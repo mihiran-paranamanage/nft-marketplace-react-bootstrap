b5cae3b8b158e3d215ed250ec219c3363ff6f635
MihiranP <mihiran.paranamana@car-care.se>
2022-04-02 15:13:17 +0530

    Add process section and styles for landing page

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -46,7 +46,6 @@ export const LandingPage: React.FC = (props) => {
             <LiveCarousel/>
 
             <h5 className="mt-5">Featured Collection</h5>
-
             <CollectionCarousel/>
 
             <h5 className="mt-5 mb-3 d-flex top-category-title">
@@ -63,7 +62,6 @@ export const LandingPage: React.FC = (props) => {
                         </Form.Select>
                     </span>
             </h5>
-
             <div className="d-flex row">
                 <div className="col-6">
                     {topCategories.slice(0, 5).map((category: string, index: number) => (
@@ -76,6 +74,99 @@ export const LandingPage: React.FC = (props) => {
                     ))}
                 </div>
             </div>
+
+            <div className="process p-5 mt-5">
+                <h5 className="mb-3">The process is simple</h5>
+                <div className="row">
+                    <div className="col-4 p-3">
+                        <img className="mb-3" src={require('../../../src/assets/process-sample.png')} alt="img" />
+                        <h6>Set up your wallet</h6>
+                        <p className="w-75">
+                            Join our waitlist to get early access to
+                            exclusive drops as soon as we set sail!
+                            Join our waitlist to get early access to
+                            exclusive drops as soon as we set sail!
+                        </p>
+                    </div>
+                    <div className="col-4 p-3">
+                        <img className="mb-3" src={require('../../../src/assets/process-sample.png')} alt="img" />
+                        <h6>Let your creative juices flowing</h6>
+                        <p className="w-75">
+                            Join our waitlist to get early access to
+                            exclusive drops as soon as we set sail!
+                            Join our waitlist to get early access to
+                            exclusive drops as soon as we set sail!
+                        </p>
+                    </div>
+                    <div className="col-4 p-3">
+                        <img className="mb-3" src={require('../../../src/assets/process-sample.png')} alt="img" />
+                        <h6>List your NFTs for sale</h6>
+                        <p className="w-75">
+                            Join our waitlist to get early access to
+                            exclusive drops as soon as we set sail!
+                            Join our waitlist to get early access to
+                            exclusive drops as soon as we set sail!
+                        </p>
+                    </div>
+                </div>
+                <div className="join-section text-center">
+                    <h4>Join our free academy and get started</h4>
+                    <p className="w-25 mx-auto">
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                    </p>
+                    <PrimaryButton color="red">Start Learning</PrimaryButton>
+                </div>
+            </div>
+
+            <h3 className="why-section text-center p-5 w-75">Why is NFT Fisher different?</h3>
+
+            <div className="row mb-5">
+                <div className="col-6 text-center">
+                    <img src={require('../../../src/assets/process-sample.png')} alt="img" />
+                </div>
+                <div className="col-6 my-auto">
+                    <h6>We care about the planet</h6>
+                    <p className="w-50">
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                    </p>
+                </div>
+            </div>
+            <div className="row mb-5">
+                <div className="col-6 my-auto text-end">
+                    <h6>We care about the planet</h6>
+                    <p className="w-50 ms-auto">
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                    </p>
+                </div>
+                <div className="col-6 text-center">
+                    <img src={require('../../../src/assets/process-sample.png')} alt="img" />
+                </div>
+            </div>
+            <div className="row mb-5">
+                <div className="col-6 text-center">
+                    <img src={require('../../../src/assets/process-sample.png')} alt="img" />
+                </div>
+                <div className="col-6 my-auto">
+                    <h6>We care about the planet</h6>
+                    <p className="w-50">
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                        Join our waitlist to get early access to
+                        exclusive drops as soon as we set sail!
+                    </p>
+                </div>
+            </div>
+
+            <div></div>
         </div>
     )
 }
