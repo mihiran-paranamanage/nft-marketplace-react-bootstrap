ced0925843ea49e23d1e3a06ace9ec80910c5be2
MihiranP <mihiran.paranamana@car-care.se>
2022-04-05 19:25:35 +0530

    Change landing page styles and navigation with navbar logo

--- src/components/shared/navbar/NavBar.tsx
+++ src/components/shared/navbar/NavBar.tsx
@@ -5,13 +5,16 @@ import {BsSearch} from 'react-icons/bs'
 import './NavBar.scss';
 
 import {PrimaryButton} from '../button/primary-button/PrimaryButton';
+import {Link} from 'react-router-dom';
 
 export const NavBar: React.FC = () => {
     return (
         <Navbar className="nav-bar" bg="light" expand="lg" sticky="top">
             <Container fluid>
                 <Navbar.Brand className="mx-5">
-                    <img src={require('../../../assets/navbar-logo.png')} alt="Logo"/>
+                    <Link className="text-decoration-none" to=''>
+                        <img src={require('../../../assets/navbar-logo.png')} alt="Logo"/>
+                    </Link>
                 </Navbar.Brand>
                 <Form className="search d-flex">
                     <BsSearch className="searchIcon"/>
