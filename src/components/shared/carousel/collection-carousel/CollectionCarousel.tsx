ba7fda9bb85a93b7b0f215d1f0ad01a66cdaa048
MihiranP <mihiran.paranamana@car-care.se>
2022-04-14 22:09:34 +0530

    Support for responsive styles

--- src/components/shared/carousel/collection-carousel/CollectionCarousel.tsx
+++ src/components/shared/carousel/collection-carousel/CollectionCarousel.tsx
@@ -7,28 +7,30 @@ import {CollectionCard} from '../../card/collection-card/CollectionCard';
 
 export const CollectionCarousel: React.FC = (props) => {
     return (
-        <Carousel className="collection-carousel" variant="dark">
-            <Carousel.Item>
-                <Carousel.Caption className="d-flex">
-                    <CollectionCard/>
-                    <CollectionCard/>
-                    <CollectionCard/>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <Carousel.Caption className="d-flex">
-                    <CollectionCard/>
-                    <CollectionCard/>
-                    <CollectionCard/>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <Carousel.Caption className="d-flex">
-                    <CollectionCard/>
-                    <CollectionCard/>
-                    <CollectionCard/>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <div className="collection-carousel-container overflow-auto">
+            <Carousel className="collection-carousel" variant="dark">
+                <Carousel.Item>
+                    <Carousel.Caption className="d-flex">
+                        <CollectionCard/>
+                        <CollectionCard/>
+                        <CollectionCard/>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Carousel.Caption className="d-flex">
+                        <CollectionCard/>
+                        <CollectionCard/>
+                        <CollectionCard/>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Carousel.Caption className="d-flex">
+                        <CollectionCard/>
+                        <CollectionCard/>
+                        <CollectionCard/>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
     )
 }
--- src/components/shared/carousel/live-carousel/LiveCarousel.tsx
+++ src/components/shared/carousel/live-carousel/LiveCarousel.tsx
@@ -7,25 +7,27 @@ import {LiveCard} from '../../card/live-card/LiveCard';
 
 export const LiveCarousel: React.FC = (props) => {
     return (
-        <Carousel className="live-carousel" variant="dark">
-            <Carousel.Item>
-                <Carousel.Caption>
-                    <LiveCard/>
-                </Carousel.Caption>
-                <img src={require('../../../../assets/carousel-background.jpg')} alt="img" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <Carousel.Caption>
-                    <LiveCard/>
-                </Carousel.Caption>
-                <img src={require('../../../../assets/carousel-background.jpg')} alt="img" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <Carousel.Caption>
-                    <LiveCard/>
-                </Carousel.Caption>
-                <img src={require('../../../../assets/carousel-background.jpg')} alt="img" />
-            </Carousel.Item>
-        </Carousel>
+        <div className="live-carousel-container overflow-auto">
+            <Carousel className="live-carousel" variant="dark">
+                <Carousel.Item>
+                    <Carousel.Caption>
+                        <LiveCard/>
+                    </Carousel.Caption>
+                    <img src={require('../../../../assets/carousel-background.jpg')} alt="img" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Carousel.Caption>
+                        <LiveCard/>
+                    </Carousel.Caption>
+                    <img src={require('../../../../assets/carousel-background.jpg')} alt="img" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Carousel.Caption>
+                        <LiveCard/>
+                    </Carousel.Caption>
+                    <img src={require('../../../../assets/carousel-background.jpg')} alt="img" />
+                </Carousel.Item>
+            </Carousel>
+        </div>
     )
 }
